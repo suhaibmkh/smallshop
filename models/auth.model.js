eddc33e7d370239bc466ab67f76f5b5a0e6047cb
suhaibmkh <80519418+suhaibmkh@users.noreply.github.com>
2021-06-06 14:34:45 +0400

    Update auth.model.js

--- models/auth.model.js
+++ models/auth.model.js
@@ -1,6 +1,10 @@
 const mongoose = require("mongoose");
-
 const bcrypt = require("bcrypt");
+
+
+
+const nodemailer = require("nodemailer");
+
 
 const DB_URL =
     "mongodb+srv://suhaib-khater_80:Galaxy-sm1@cluster0.vzcs0.mongodb.net/online-shop?retryWrites=true&w=majority";
@@ -13,8 +17,20 @@ const userSchema = mongoose.Schema({
         default: false
     }
 });
-
 const User = mongoose.model("acct", userSchema);
+
+
+var smtpTransport = nodemailer.createTransport({
+    service: "Gmail",
+    auth: {
+        user: "suhaib0@gmail.com",
+        pass: "Galaxy-sM1"
+    }
+});
+var rand,mailOptions,host,link;
+
+
+
 
 exports.createNewUser = (username, email, password) => {
     return new Promise((resolve, reject) => {
@@ -28,6 +44,26 @@ exports.createNewUser = (username, email, password) => {
                     mongoose.disconnect();
                     reject("email is used");
                 } else {
+                     rand=Math.floor((Math.random() * 100) + 54);
+    host=req.get('host');
+    link="http://"+req.get('host')+"/verify?id="+rand;
+    mailOptions={
+        to : req.body.email,
+        subject : "Please confirm your Email account",
+        html : "Hello,<br> Please Click on the link to verify your email.<br><a href="+link+">Click here to verify</a>"
+    }
+    console.log(mailOptions);
+    smtpTransport.sendMail(mailOptions, function(error, response){
+     if(error){
+            console.log(error);
+        res.end("error");
+     }else{
+            console.log("Message sent: " + response.message);
+        res.end("sent");
+         }
+});
+                    
+                    
                     return bcrypt.hash(password, 10);
                 }
             })
