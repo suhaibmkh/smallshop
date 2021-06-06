e629994ebe4518237dc9ffc9003ee98b7cc009ea
suhaibmkh <80519418+suhaibmkh@users.noreply.github.com>
2021-06-06 16:28:40 +0400

    Update auth.model.js

--- models/auth.model.js
+++ models/auth.model.js
@@ -1,35 +1,21 @@
 const mongoose = require("mongoose");
 
 const bcrypt = require("bcrypt");
-const crypto = require('crypto');
-const nodemailer = require('nodemailer');
 
 const DB_URL =
     "mongodb+srv://suhaib-khater_80:Galaxy-sm1@cluster0.vzcs0.mongodb.net/online-shop?retryWrites=true&w=majority";
 const userSchema = mongoose.Schema({
     username: String,
-  
+    email: String,
     password: String,
     isAdmin: {
         type: Boolean,
         default: false
-    },
-   
-    email: { type: String, unique: true },
-    roles: [{ type: 'String' }],
-    isVerified: { type: Boolean, default: false },
-  
-    passwordResetToken: String,
-    passwordResetExpires: Date
-});
-const tokenSchema = new mongoose.Schema({
-    _userId: { type: mongoose.Schema.Types.ObjectId, required: true, ref: 'User' },
-    token: { type: String, required: true },
-    createdAt: { type: Date, required: true, default: Date.now, expires: 43200 }
+    }
 });
 
-const User = mongoose.model("user", userSchema);
-const Token = mongoose.model("token", tokenSchema);
+const User = mongoose.model("acct", userSchema);
+
 exports.createNewUser = (username, email, password) => {
     return new Promise((resolve, reject) => {
         mongoose
@@ -51,24 +37,7 @@ exports.createNewUser = (username, email, password) => {
                     email: email,
                     password: hashedPassword
                 });
-                user.save(function (err) {
-                    if (err) { return res.status(500).send({ msg: err.message }); }
-             
-                    // Create a verification token for this user
-                    var token = new Token({ _userId: user._id, token: crypto.randomBytes(16).toString('hex') });
-             
-                    // Save the verification token
-                    token.save(function (err) {
-                        if (err) { return res.status(500).send({ msg: err.message }); }
-             
-                        // Send the email
-                        var transporter = nodemailer.createTransport({ service: 'Sendgrid', auth: { user: process.env.SENDGRID_USERNAME, pass: process.env.SENDGRID_PASSWORD } });
-                        var mailOptions = { from: 'no-reply@yourwebapplication.com', to: user.email, subject: 'Account Verification Token', text: 'Hello,\n\n' + 'Please verify your account by clicking the link: \nhttp:\/\/' + req.headers.host + '\/confirmation\/' + token.token + '.\n' };
-                        transporter.sendMail(mailOptions, function (err) {
-                            if (err) { return res.status(500).send({ msg: err.message }); }
-                            res.status(200).send('A verification email has been sent to ' + user.email + '.');
-                        });
-                    });
+                return user.save();
             })
             .then(() => {
                 mongoose.disconnect();
@@ -79,10 +48,7 @@ exports.createNewUser = (username, email, password) => {
                 reject(err);
             });
     });
-});
 };
-
-
 
 exports.login = (email, password) => {
     return new Promise((resolve, reject) => {
