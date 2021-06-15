fafc3c70cbbf48b6c9a26eec1a33a4e87923496f
suhaibmkh <80519418+suhaibmkh@users.noreply.github.com>
2021-06-15 11:23:04 +0400

    Add files via upload

--- routes/admin.route.js
+++ routes/admin.route.js
@@ -16,31 +16,32 @@ router.post(
             destination: (req, file, cb) => {
                 cb(null, "images/");
             },
-            filename: (req, file, cb) => {
-                cb(null, Date.now() + "-" + file.originalname);
+            filename: (req, files, cb) => {
+                cb(null, Date.now() + "-" + files.originalname);
+
             }
         })
-    }).single("image"),
+    }).array("image", 5),
     check("name")
-        .not()
-        .isEmpty()
-        .withMessage("name is required"),
+    .not()
+    .isEmpty()
+    .withMessage("name is required"),
     check("price")
-        .not()
-        .isEmpty()
-        .withMessage("price is required")
-        .isFloat({ min: 0.0000000009 })
-        .withMessage("price must be greater than 0"),
+    .not()
+    .isEmpty()
+    .withMessage("price is required")
+    .isFloat({ min: 0.0000000009 })
+    .withMessage("price must be greater than 0"),
     check("description")
-        .not()
-        .isEmpty()
-        .withMessage("description is required"),
+    .not()
+    .isEmpty()
+    .withMessage("description is required"),
     check("category")
-        .not()
-        .isEmpty()
-        .withMessage("category is required"),
+    .not()
+    .isEmpty()
+    .withMessage("category is required"),
     check("image").custom((value, { req }) => {
-        if (req.file) return true;
+        if (req.files) return true;
         else throw "image is required";
     }),
     adminController.postAdd
