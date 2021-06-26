0ead0a00dde5b5587418ecd896e38b7b9ca7e3d5
suhaibmkh <80519418+suhaibmkh@users.noreply.github.com>
2021-06-26 13:41:53 +0400

    Add files via upload

--- app.js
+++ app.js
@@ -11,6 +11,20 @@ const authRouter = require("./routes/auth.route");
 const cartRouter = require("./routes/cart.route");
 const adminRouter = require("./routes/admin.route");
 const orderRouter = require("./routes/orders.route");
+const shippingRouter = require("./routes/shipping.route");
+const confirmAddressRouter = require("./routes/confirmAddress.route")
+const payRoute = require("./routes/pay.route")
+
+const paypal = require("paypal-rest-sdk");
+const order = require("./models/order.model")
+
+paypal.configure({
+    mode: "sandbox", //sandbox or live
+    client_id: "ASio2urN0x9nZ65ki6cajANsdrFp6FePrQsywTX3O5rYeFonwzA5KSEkwy7612pCl6W97DAvlCDvEyGe",
+    client_secret: "ENXthIPOAJCpilzz2GiYrM3tWroAqavJSgl_y_w9AMngSheEhS63vbwf3XP1JZhUuxbOr3HGOJ3jj2Ki",
+});
+
+
 
 const app = express();
 
@@ -19,8 +33,7 @@ app.use(express.static(path.join(__dirname, "images")));
 app.use(flash());
 
 const STORE = new SessionStore({
-    uri:
-        "mongodb+srv://suhaib-khater_80:Galaxy-sm1@cluster0.vzcs0.mongodb.net/online-shop?retryWrites=true&w=majority",
+    uri: "mongodb+srv://suhaib-khater_80:Galaxy-sm1@cluster0.vzcs0.mongodb.net/online-shop?retryWrites=true&w=majority",
     collection: "sessions"
 });
 
@@ -41,6 +54,43 @@ app.use("/product", productRouter);
 app.use("/cart", cartRouter);
 app.use("/admin", adminRouter);
 app.use("/", orderRouter);
+app.use("/shippingaddress", shippingRouter);
+app.use("/confirmaddress", confirmAddressRouter);
+app.use("/update", confirmAddressRouter);
+app.use("/pay", payRoute)
+app.get("/success", (req, res, next) => {
+
+    const payerId = req.query.PayerID;
+    const paymentId = req.query.paymentId;
+
+    const execute_payment_json = {
+        payer_id: payerId,
+        transactions: [{
+            amount: {
+                currency: "USD",
+                total: req.session.sum,
+            },
+        }, ],
+    };
+
+    paypal.payment.execute(paymentId, execute_payment_json, function(
+        error,
+        payment
+    ) {
+        if (error) {
+            console.log(error.response);
+            throw error;
+        } else {
+            console.log(JSON.stringify(payment));
+            console.log(req.session.id);
+            order.updatePay(req.session.orderId)
+            res.redirect("/orders")
+        }
+    });
+
+});
+
+app.get("/cancel", (req, res) => res.send("Cancelled"));
 
 app.get("/error", (req, res, next) => {
     res.status(500);
