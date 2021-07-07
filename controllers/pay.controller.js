10150d201caa200b3574e627ff11bfcabcb951bc
suhaibmkh <80519418+suhaibmkh@users.noreply.github.com>
2021-07-07 10:56:21 +0400

    Update pay.controller.js

--- controllers/pay.controller.js
+++ controllers/pay.controller.js
@@ -61,7 +61,7 @@ const paypal = require("paypal-rest-sdk");
 	                                    "line2": shipp.address2,
 	                                    "city": shipp.city,
 	                                    "country_code": "US",
-	                                    "postal_code": shipp.zip,
+	                                    "postal_code": "18117-134",
 	                                    "state": shipp.state,
 	                                    "phone": shipp.phone[0]
 	                                },
