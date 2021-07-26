e1a1e2f3471078cfba7eb60ca723e3cb268affed
suhaibmkh <80519418+suhaibmkh@users.noreply.github.com>
2021-07-26 11:44:50 +0400

    Update auth.controller.js

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -67,7 +67,7 @@ exports.activereset = (req, res, next) => {
     );
 
     oauth2Client.setCredentials({
-        refresh_token:  "1//04T_nqlj9UVrVCgYIARAAGAQSNwF-L9IrGm-NOdEKBOakzMn1cbbCHgg2ivkad3Q_hMyBkSQen0b5ABfR8kPR18aOoqhRrSlPm9w",
+        refresh_token: "1//04T_nqlj9UVrVCgYIARAAGAQSNwF-L9IrGm-NOdEKBOakzMn1cbbCHgg2ivkad3Q_hMyBkSQen0b5ABfR8kPR18aOoqhRrSlPm9w",
     });
     const accessToken = oauth2Client.getAccessToken()
     const email = req.body.email
@@ -85,7 +85,7 @@ exports.activereset = (req, res, next) => {
             user: "nodejsa@gmail.com",
             clientId: "173872994719-pvsnau5mbj47h0c6ea6ojrl7gjqq1908.apps.googleusercontent.com",
             clientSecret: "OKXIYR14wBB_zumf30EC__iJ",
-            refreshToken:  "1//04T_nqlj9UVrVCgYIARAAGAQSNwF-L9IrGm-NOdEKBOakzMn1cbbCHgg2ivkad3Q_hMyBkSQen0b5ABfR8kPR18aOoqhRrSlPm9w",
+            refreshToken: "1//04T_nqlj9UVrVCgYIARAAGAQSNwF-L9IrGm-NOdEKBOakzMn1cbbCHgg2ivkad3Q_hMyBkSQen0b5ABfR8kPR18aOoqhRrSlPm9w",
 
         },
     });
