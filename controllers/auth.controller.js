68b074194912c6d32c02ba28fbcb87c3224f876d
suhaibmkh <80519418+suhaibmkh@users.noreply.github.com>
2021-07-26 11:50:24 +0400

    Update auth.controller.js

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -62,12 +62,12 @@ exports.activereset = (req, res, next) => {
 
     const oauth2Client = new OAuth2(
         "173872994719-pvsnau5mbj47h0c6ea6ojrl7gjqq1908.apps.googleusercontent.com", // ClientID
-        "OKXIYR14wBB_zumf30EC__iJ",, // Client Secret
+        "OKXIYR14wBB_zumf30EC__iJ", // Client Secret
         "https://developers.google.com/oauthplayground" // Redirect URL
     );
 
     oauth2Client.setCredentials({
-        refresh_token: "1//04T_nqlj9UVrVCgYIARAAGAQSNwF-L9IrGm-NOdEKBOakzMn1cbbCHgg2ivkad3Q_hMyBkSQen0b5ABfR8kPR18aOoqhRrSlPm9w",
+        refresh_token: "1//04T_nqlj9UVrVCgYIARAAGAQSNwF-L9IrGm-NOdEKBOakzMn1cbbCHgg2ivkad3Q_hMyBkSQen0b5ABfR8kPR18aOoqhRrSlPm9w"
     });
     const accessToken = oauth2Client.getAccessToken()
     const email = req.body.email
@@ -90,7 +90,7 @@ exports.activereset = (req, res, next) => {
         },
     });
     const mailOptions = {
-        from: '"Auth Admin" <onlineshopkeyboard@gmail.com>', // sender address
+        from: '"Auth Admin" <nodejsa@gmail.com>', // sender address
         to: email, // list of receivers
         subject: "Resert Password ✔", // Subject line
         generateTextFromHTML: true,
