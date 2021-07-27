60ff45d4fd84603cd81fb214db91399491ff853c
suhaibmkh <80519418+suhaibmkh@users.noreply.github.com>
2021-07-27 14:03:32 +0400

    Update auth.controller.js

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -80,18 +80,20 @@ exports.activereset = (req, res, next) => {
     const link = host + "/verify/verifyreset?id=" + token
 
     const transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-            type: "OAuth2",
-            user: "nodejsa@gmail.com",
-            clientId: "173872994719-pvsnau5mbj47h0c6ea6ojrl7gjqq1908.apps.googleusercontent.com",
-            clientSecret: "OKXIYR14wBB_zumf30EC__iJ",
+        host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
+       auth: {
+        type: 'OAuth2',
+        
+            user: "onlineshopkeyboard@gmail.com",
+           
             refreshToken: "1//04T_nqlj9UVrVCgYIARAAGAQSNwF-L9IrGm-NOdEKBOakzMn1cbbCHgg2ivkad3Q_hMyBkSQen0b5ABfR8kPR18aOoqhRrSlPm9w",
 
         },
     });
     const mailOptions = {
-        from: '"Auth Admin" <nodejsa@gmail.com>', // sender address
+        from: '"Auth Admin" <onlineshopkeyboard@gmail.com>', // sender address
         to: email, // list of receivers
         subject: "Resert Password ✔", // Subject line
         generateTextFromHTML: true,
